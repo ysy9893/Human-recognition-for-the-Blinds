55994fa0fe4eb0442a64a7bb1448b916ea585a82
Chatherina423 <ysh9473@naver.com>
2021-04-08 00:43:51 +0900

    Added Gesture Recog API MEDIAPIPE (Modified the Landmark Print)

---
+++ Basic_Mediapipe.py
@@ -0,0 +1,101 @@
+import cv2
+import mediapipe as mp
+import copy
+import itertools
+
+mp_drawing = mp.solutions.drawing_utils
+mp_hands = mp.solutions.hands
+
+
+def pre_process_landmark(landmark_list):
+  temp_landmark_list = copy.deepcopy(landmark_list)
+
+  # Convert to relative coordinates
+  base_x, base_y = 0, 0
+  for index, landmark_point in enumerate(temp_landmark_list):
+    if index == 0:
+      base_x, base_y = landmark_point[0], landmark_point[1]
+
+    temp_landmark_list[index][0] = temp_landmark_list[index][0] - base_x
+    temp_landmark_list[index][1] = temp_landmark_list[index][1] - base_y
+
+  # Convert to a one-dimensional list
+  temp_landmark_list = list(
+    itertools.chain.from_iterable(temp_landmark_list))
+
+  # Normalization
+  max_value = max(list(map(abs, temp_landmark_list)))
+
+  def normalize_(n):
+    return n / max_value
+
+  temp_landmark_list = list(map(normalize_, temp_landmark_list))
+
+  return temp_landmark_list
+
+# 함수
+def calc_landmark_list(image, landmarks):
+  image_width, image_height = image.shape[1], image.shape[0]
+
+  landmark_point = []
+
+  # Keypoint
+  for _, landmark in enumerate(landmarks.landmark):
+    landmark_x = min(int(landmark.x * image_width), image_width - 1)
+    landmark_y = min(int(landmark.y * image_height), image_height - 1)
+    # landmark_z = landmark.z
+
+    landmark_point.append([landmark_x, landmark_y])
+
+  return landmark_point
+
+
+
+
+
+# For webcam input:
+cap = cv2.VideoCapture(0)
+with mp_hands.Hands(
+    min_detection_confidence=0.5,
+    min_tracking_confidence=0.5) as hands:
+  while cap.isOpened():
+    success, image = cap.read()
+    if not success:
+      print("Ignoring empty camera frame.")
+      # If loading a video, use 'break' instead of 'continue'.
+      continue
+
+    # Flip the image horizontally for a later selfie-view display, and convert
+    # the BGR image to RGB.
+    image = cv2.cvtColor(cv2.flip(image, 1), cv2.COLOR_BGR2RGB)
+    # To improve performance, optionally mark the image as not writeable to
+    # pass by reference.
+    image.flags.writeable = False
+    results = hands.process(image)
+
+    # Draw the hand annotations on the image.
+    image.flags.writeable = True
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+
+    if results.multi_hand_landmarks:
+      for hand_landmarks in results.multi_hand_landmarks:
+        mp_drawing.draw_landmarks(
+            image, hand_landmarks, mp_hands.HAND_CONNECTIONS)
+
+      landmark_list = calc_landmark_list(image, hand_landmarks)
+      pre_processed_landmark_list = pre_process_landmark(
+        landmark_list)
+
+      # print landmark list (1D)
+
+      print(pre_processed_landmark_list)
+
+
+
+    cv2.imshow('MediaPipe Hands', image)
+    if cv2.waitKey(5) & 0xFF == 27:
+      break
+cap.release()
+
+
+
